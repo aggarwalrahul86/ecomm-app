16966a7bae6160997129a484d6101343d24873a6
Sonia Raychaudhuri <sonia2305@gmail.com>
2016-03-04 15:59:52 +0530

    Frame images

--- app/components/store/MainController.js
+++ app/components/store/MainController.js
@@ -1,8 +1,11 @@
-app.controller("MainController", function($scope, $http, filters, productService, DataService,$location) {
+app.controller("MainController", function($scope, $http, filters, productService, DataService, $location, $anchorScroll) {
 
 					$scope.appStyle = "default";
 					$scope.filters = filters;
 					$scope.contextPath = $location.absUrl().substr(0, $location.absUrl().lastIndexOf("#"));
+					$scope.imagePath = $scope.contextPath + "assets/img/products/";
+					$scope.imageExtension = ".jpg";
+					$scope.bannerImagePath = $scope.contextPath + "assets/img/banners/";
 
 					$scope.myModel = {
 						Url : 'http://jasonwatmore.com/post/2014/08/01/AngularJS-directives-for-social-sharing-buttons-Facebook-Like-GooglePlus-Twitter-and-Pinterest.aspx',
@@ -35,5 +38,10 @@ app.controller("MainController", function($scope, $http, filters, productService
 					.success(function(response) {
 						$scope.mainSliders = response.mainSliders;
 				});
+					
+				$scope.scrollTo = function(id) {
+				      $location.hash(id);
+				      $anchorScroll();
+				   }
 
-				});
+});
