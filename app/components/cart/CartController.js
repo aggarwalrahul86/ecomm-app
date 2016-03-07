0c65ae0314cce07603db1e416d4d6462f30dae71
eaggara <rahul.a.aggarwal@ericsson.com>
2016-03-07 17:40:24 +0530

    cart

--- app/components/cart/CartController.js
+++ app/components/cart/CartController.js
@@ -12,4 +12,40 @@ app.controller("CartController", function($scope,$http) {
         $scope.TabNumber = index;
     }
 	
+	$scope.sendEmail = function(){
+		
+		data = {};
+		
+		data.body = "this is checking put of details";
+		data.from = "aggarwalrahul86@gmail.com";
+		data.to = "aggarwalrahul86@gmail.com";
+		data.fromName = "Rahul aggarwal";
+		
+		
+		$http.post("/api/sendemail", data,{
+	        headers: { 'Content-Type': 'application/json'}
+	    })
+			.success(function (data, status, headers, config) {
+				console.log("Response Recieved",data.status);
+				
+				if(data.status == "Message sent"){
+					console.log("Email sent");
+					
+					//$scope.visitor.isCustomer = "true" ;
+					//$scope.visitor.message = "Message Sent";
+					
+				}else{
+					//$scope.visitor.isCustomer = "false" ;
+					//$scope.visitor.message = "Login is not successful. Welcome Guest";
+				}
+				
+        	})
+	        .error(function (data, status, headers, config) {
+	        	$scope.visitor.isCustomer = "false" ;
+				$scope.visitor.message = "Login is not successful. Welcome Guest";
+	        	 console.log("error Response ", data);
+	        });
+		
+	}
+	
 });
